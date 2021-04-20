6c995d68ed3e606c0edcc58b8d589d4521c5ba1e
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-04-20 09:28:26 +0100

    add questions

---
+++ lib/app_functionality.dart
@@ -0,0 +1,44 @@
+import 'questions.dart';
+
+List<Questions> questionsList = [
+  Questions(
+      questionText: "هل الإرض تدور حول الشمس",
+      questionImage: "assets/images/image-1.jpg",
+      result: true),
+  Questions(
+      questionText: "أكثر منطقة منخفضة في العالم هو البحر الميت",
+      questionImage: "assets/images/image-1.jpg",
+      result: true),
+  Questions(
+      questionText: "زوجة فرعون هي السيدة آسيا",
+      questionImage: "assets/images/image-1.jpg",
+      result: true),
+  Questions(
+      questionText: "درجة غليان الماء هي 80 درجة",
+      questionImage: "assets/images/image-1.jpg",
+      result: false),
+  Questions(
+      questionText: " إسم أنثى الحمار أتان",
+      questionImage: "assets/images/image-1.jpg",
+      result: true),
+  Questions(
+      questionText: "هل الدجاجة تطير",
+      questionImage: "assets/images/image-1.jpg",
+      result: false),
+  Questions(
+      questionText: "تقع كوريا الجنوبية في قارة أمريكا الجنوبية",
+      questionImage: "assets/images/image-1.jpg",
+      result: false),
+  Questions(
+      questionText: "أطول سورة في القرآن هي سورة النساء",
+      questionImage: "assets/images/image-1.jpg",
+      result: false),
+  Questions(
+      questionText: "ثاني خليفة في الاسلام هو سيدنا عثمان",
+      questionImage: "assets/images/image-1.jpg",
+      result: false),
+  Questions(
+      questionText: "آخر دولة خلافة إسلامية هي الدولة العثمانية",
+      questionImage: "assets/images/image-1.jpg",
+      result: true)
+];
--- lib/questions.dart
+++ lib/questions.dart
@@ -1,5 +1,11 @@
 class Questions {
-  String question;
-  
+  String questionText;
+  String questionImage;
   bool result;
+
+  Questions({this.questionText, this.questionImage, this.result }) {
+    // this.questionText = questionText;
+    // this.questionImage = questionImage;
+    // this.result = result;
+  }
 }
